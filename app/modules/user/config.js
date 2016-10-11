6777f7282a3079096f4e35c790696c99f6d00eeb
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-10-10 23:53:25 -0500

    piecing together template

--- app/modules/user/config.js
+++ app/modules/user/config.js
@@ -1,15 +1,16 @@
 function config($stateProvider) {
     $stateProvider
-    .state("register", {
-        url:"/",
-        controller: "RegisterController as registerCtrl",
-        template: require("./views/register.html")
-    })
     .state("profile", {
         url:"/",
         controller: "ProfileController as profileCtrl",
         template: require("./views/profile.html")
     })
+    .state("register", {
+        url:"/",
+        controller: "RegisterController as registerCtrl",
+        template: require("./views/register.html")
+    })
+    
 }
 
 export default config;
